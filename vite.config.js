5295c9e5e41be9e135390b5f999c3f09714a83fc
pragna <pragna78504@gmail.com>
2025-08-02 16:33:13 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/ecommerce/',
+  build: {
+    outDir: 'dist',   // ensure dist folder is created
+    emptyOutDir: true
+  }
 })
